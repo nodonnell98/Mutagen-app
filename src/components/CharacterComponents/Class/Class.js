219b879baa8bfcab6bcb162d742ce60180e32d9e
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-08-01 18:08:47 +0100

    changed class stats display

--- src/components/CharacterComponents/Class/Class.js
+++ src/components/CharacterComponents/Class/Class.js
@@ -16,9 +16,9 @@ export default function Class(props) {
     <div className="flexBoxColumn" >
       <h1 style={classInfoStyle} className="flexGrow1">{classification.name}</h1>
       <h3 style={classInfoStyle } className="flexGrow1">{classification.description}</h3>
-      <h3 style={classInfoStyle } className="flexGrow1">Strike Stat: {classification.strike_stat}</h3>
-      <h3 style={classInfoStyle } className="flexGrow1">Might Stat: {classification.might_stat}</h3>
-      <h3 style={classInfoStyle } className="flexGrow1">Dodge Stat: {classification.dodge_stat}</h3>
+      <h3 style={classInfoStyle } className="flexGrow1">Strike: {classification.strike_stat.toUpperCase()}</h3>
+      <h3 style={classInfoStyle } className="flexGrow1">Might: {classification.might_stat.toUpperCase()}</h3>
+      <h3 style={classInfoStyle } className="flexGrow1">Dodge: {classification.dodge_stat.toUpperCase()}</h3>
       <h3 style={classInfoStyle } className="flexGrow1">Gene Pool: {classification.gene_pool}</h3>
 
 
