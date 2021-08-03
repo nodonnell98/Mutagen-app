89406554a3d5e2671061b1c5d8948e77c3f50771
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-08-03 21:40:12 +0100

    cannot access my characters if not logged in

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Weapons from './components/pages/Weapons';
 import Header from './components/layout/Header';
 import CreateWeapon from './components/WeaponComponents/CreateWeapon';
+import authenticationService from './services/authentication.service';
 import LoginForm from './components/authentication/LoginForm'
 import RegisterForm from './components/authentication/RegisterForm'
 import ComingSoon from './components/pages/ComingSoon';
@@ -24,7 +25,7 @@ class App extends Component {
           <Route exact path="/login" ><LoginForm /></Route>
           <Route exact path="/register" ><RegisterForm /></Route>
           <Route exact path="/coming_soon" ><ComingSoon /></Route>
-          <Route exact path="/characters" ><Characters /></Route>
+          <Route exact path="/characters" >{authenticationService.loggedIn() ? <Characters /> : <LoginForm />}</Route>
           <Route exact path="/character/:id" render={(props) =>  <Character id={props}/>}></Route>
 
             </div>
