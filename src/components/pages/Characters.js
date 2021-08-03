962e891977ec0b1e8d38d18e856e8095ed60007e
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-08-03 21:42:55 +0100

    fixed no hover for create character card

--- src/components/pages/Characters.js
+++ src/components/pages/Characters.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect, useCallback } from "react";
 import CharacterService from "../../services/character.service";
 import ClassificationService from "../../services/classification.service";
 import CharacterCard from "../CharacterComponents/CharacterCard";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import CharacterForm from "../CharacterComponents/CharacterForm";
-import '../../App.css'
+import "../../App.css";
 
 export default function Characters() {
   const [characters, setCharacters] = useState([]);
-  const [classification_id, setClassificationId] = useState()
-  const [classifications, setClassifications] = useState([])
+  const [classification_id, setClassificationId] = useState();
+  const [classifications, setClassifications] = useState([]);
 
   const containerStyle = {
     display: "flex",
@@ -38,8 +38,8 @@ export default function Characters() {
     width: "20em",
     boxShadow: "-1px 0px 18px 0px rgb(18,94,100)", //rgb(62,170,184)
     cursor: "pointer",
-    transition: '0.3s'
-  }
+    transition: "0.3s",
+  };
 
   const retrieveCharactersInformation = useCallback(() => {
     CharacterService.index().then((response) => {
@@ -55,18 +55,33 @@ export default function Characters() {
     retrieveCharactersInformation();
   }, [retrieveCharactersInformation]);
 
-
   return (
     <div style={containerStyle}>
-       <div style={cardContainerStyle} >
-        <Card style={cardStyle} class="noHover"><CharacterForm classification_id={classification_id} classifications={classifications} setClassificationId={setClassificationId}></CharacterForm></Card>
+      <div style={cardContainerStyle}>
+        <Card style={cardStyle} className="noHover">
+          <CharacterForm
+            classification_id={classification_id}
+            classifications={classifications}
+            setClassificationId={setClassificationId}
+          ></CharacterForm>
+        </Card>
       </div>
       {characters.map((char, i) => {
-         return (<div style={cardContainerStyle} >
-          <Link to={"/character/" + char.id}><CharacterCard style={cardStyle} key={i} character={char} classifications={classifications} cardStyle={cardStyle} className="cardHover"></CharacterCard></ Link>
-        </div>)
+        return (
+          <div style={cardContainerStyle}>
+            <Link to={"/character/" + char.id}>
+              <CharacterCard
+                style={cardStyle}
+                key={i}
+                character={char}
+                classifications={classifications}
+                cardStyle={cardStyle}
+                className="cardHover"
+              ></CharacterCard>
+            </Link>
+          </div>
+        );
       })}
-
     </div>
   );
 }
