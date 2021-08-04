f5e6b622f0bbe89955ac41f7b8bf093439fb0f62
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-08-04 23:12:41 +0100

    got all skils to display

--- src/components/CharacterComponents/Stats/Skills.js
+++ src/components/CharacterComponents/Stats/Skills.js
@@ -1,60 +1,77 @@
 import React from "react";
 
 export default function Skills(props) {
-
   const character = props.character;
   const skillCategoryKeys = Object.keys(character.skills);
 
   const skillStyle = {
     // borderBottom: '1px solid #66FCF1',
-  }
+  };
 
   const skillsContainerStyle = {
-    height: '600px',
-    overflow: 'scroll',
-    marginRight: '2%',
-    marginLeft: '2%'
-  }
+    height: "600px",
+    overflow: "scroll",
+    marginRight: "2%",
+    marginLeft: "2%",
+    display: 'block'
+  };
 
   function profStyle(value) {
     const prof = {
-      color: '#33ff33'
-    }
+      color: "#33ff33",
+    };
 
     const expert = {
-      color: '#ffa31a'
-    }
+      color: "#ffa31a",
+    };
     if (value == 1) {
-      return prof
+      return prof;
     } else if (value == 2) {
-      return expert
+      return expert;
     } else {
-      return
+      return;
     }
   }
 
   function getProficiency(value) {
     if (value == 1) {
-      return 'Proficient'
+      return "Proficient";
     } else if (value == 2) {
-      return 'Expert'
+      return "Expert";
     } else {
-      return 'Not Proficient'
+      return "Not Proficient";
     }
   }
 
   return (
-    <div className="container flexBoxColumn flexGrow1" style={skillsContainerStyle}>
+    <div
+      className="container flexBoxColumn flexGrow1"
+      style={skillsContainerStyle}
+    >
       {skillCategoryKeys.map((skillCategory, i) => {
         let skillKeys = Object.keys(character.skills[skillCategory]);
         return skillKeys.map((skill, i) => {
           return (
             <div>
-            <div className={"flexBoxRow flexGrow1"} style={skillStyle} key={i}>
-              <p className="flexGrow1">{skill.toUpperCase()}</p>
-              <p className="flexGrow1" style={profStyle(character.skills[skillCategory][skill])}>{getProficiency(character.skills[skillCategory][skill])}</p>
-            </div>
-            <div className="flexGrow1" style={{borderBottom: '1px solid #66FCF1', height: '1px'}}> </div>
+              <div
+                className={"flexBoxRow flexGrow1"}
+                style={skillStyle}
+                key={i}
+              >
+                <p className="flexGrow1">{skill.toUpperCase()}</p>
+                <p
+                  className="flexGrow1"
+                  style={profStyle(character.skills[skillCategory][skill])}
+                >
+                  {getProficiency(character.skills[skillCategory][skill])}
+                </p>
+              </div>
+              <div
+                className="flexGrow1"
+                style={{ borderBottom: "1px solid #66FCF1", height: "1px" }}
+              >
+                {" "}
+              </div>
             </div>
           );
         });
