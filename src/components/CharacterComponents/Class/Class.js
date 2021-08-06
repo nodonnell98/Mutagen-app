aacee6b492e9af58c152daf7b2545ba40054570e
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-08-06 11:20:57 +0100

    foxed class uppercase

--- src/components/CharacterComponents/Class/Class.js
+++ src/components/CharacterComponents/Class/Class.js
@@ -3,7 +3,6 @@ import React, { useState, useCallback, useEffect } from 'react'
 
 export default function Class(props) {
 
-  const character = props.character
   const classification = props.classification
 
   const classInfoStyle = {
@@ -16,9 +15,9 @@ export default function Class(props) {
     <div className="flexBoxColumn" >
       <h1 style={classInfoStyle} className="flexGrow1">{classification.name}</h1>
       <h3 style={classInfoStyle } className="flexGrow1">{classification.description}</h3>
-      <h3 style={classInfoStyle } className="flexGrow1">Strike: {classification.strike_stat.toUpperCase()}</h3>
-      <h3 style={classInfoStyle } className="flexGrow1">Might: {classification.might_stat.toUpperCase()}</h3>
-      <h3 style={classInfoStyle } className="flexGrow1">Dodge: {classification.dodge_stat.toUpperCase()}</h3>
+      <h3 style={classInfoStyle } className="flexGrow1">Strike: {classification.strike_stat ? classification.strike_stat.toUpperCase() :  classification.strike_stat}</h3>
+      <h3 style={classInfoStyle } className="flexGrow1">Might: {classification.might_stat ? classification.might_stat.toUpperCase() :  classification.might_stat}</h3>
+      <h3 style={classInfoStyle } className="flexGrow1">Dodge: {classification.dodge_stat ? classification.dodge_stat.toUpperCase() :  classification.dodge_stat}</h3>
       <h3 style={classInfoStyle } className="flexGrow1">Gene Pool: {classification.gene_pool}</h3>
 
 
