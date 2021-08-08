e34284fe39a423f5fd531a26081bb655885cad6d
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-08-08 13:25:41 +0100

    adding padding to coming soon

--- src/components/pages/ComingSoon.js
+++ src/components/pages/ComingSoon.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 export default function ComingSoon() {
   return (
-    <div>
-      <h1 style={{color: '#66FCF1'}}>This page is coming soon</h1>
+    <div style={{marginTop: '10vh'}}>
+      <h1 style={{color: '#71f1e8'}}>This page is coming soon</h1>
     </div>
   );
 
